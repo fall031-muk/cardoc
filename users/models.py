53a7459e3579d36611018ac6a365037746daef8f
fall031 <fall900802@gmail.com>
2021-11-23 13:16:52 +0900

    Feat: 회원가입/로그인

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db                     import models
+from django.contrib.auth.hashers   import make_password
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models    import UserManager
+
+class CustomUserManagoer(UserManager):
+    def _create_user(self, userid, password, **extra_field):
+        if not userid:
+            raise ValueError("input user_id")
+        user          = self.model(userid=userid, **extra_field)
+        user.password = make_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, userid, password=None, **extra_fields):
+        return self._create_user(userid, password, **extra_fields)
+        
+
+class User(AbstractBaseUser):
+    userid    = models.CharField(max_length=30, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'userid'
+    objects = CustomUserManagoer()
---
+++ users/views.py
@@ -0,0 +1,9 @@
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import permissions
+from .models import User
+from .serializers import UserSerializer
+
+class UserViewSet(CreateModelMixin, GenericViewSet):
+    queryset         = User.objects.all()
+    serializer_class = UserSerializer
